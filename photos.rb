5915a87502ecbc2d452badff893cff1513bc74c4
Patrick <nyatindopatrick@gmail.com>
2020-07-18 23:42:03 +0300

    refactor code

--- photos.rb
+++ photos.rb
@@ -1,19 +1,6 @@
-def take_pics(num)
-  return 1 if num == 1
-  max = num
-  even = (1...max).count { |n| n.even? }
-  odd = even = (1...max).count { |n| n.odd? }
-  count = 0
-  while max > 1
-    if num.odd?
-      count += even 
-      max -= 1
-    else
-      count += odd 
-      max -= 1
-    end
-  end
-  count 
+def take_pics(even, odd)
+  odds = (0...odd).map { |x| x * 2 + 1 }.size
+  evens = (0...even).map { |x| x * 2 + 2 }.size
+  odds * evens + 2
 end
-
-print take_pics(10)
+print take_pics(2, 3)
