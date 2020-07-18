2f79d5c5c43acf320dcb99a3dcd199d1640c7395
Patrick <nyatindopatrick@gmail.com>
2020-07-18 23:43:50 +0300

    refactor code

--- photos.rb
+++ photos.rb
@@ -1,6 +1,8 @@
 def take_pics(even, odd)
   odds = (0...odd).map { |x| x * 2 + 1 }.size
   evens = (0...even).map { |x| x * 2 + 2 }.size
+  # the 2 is added because the last value is the added with all odds/evens
   odds * evens + 2
 end
+
 print take_pics(2, 3)
