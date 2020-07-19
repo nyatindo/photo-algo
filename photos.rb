7750611b3f29c9f2c4e0632cd24d2528474e7d73
Patrick <nyatindopatrick@gmail.com>
2020-07-19 08:17:43 +0300

    add more conditions

--- photos.rb
+++ photos.rb
@@ -1,8 +1,8 @@
 def take_pics(even, odd)
   odds = (0...odd).map { |x| x * 2 + 1 }.size 
   evens = (0...even).map { |x| x * 2 + 2 }.size
-  # the 2 is added because the last value is the added with all odds/evens
-  (odds > 1 ? odds + 1 : odds) * (evens > 1 ? evens  + 1 : evens)
+  # the 1 is added because the last value is the added with all odds/evens
+  (odds > 1 && evens > 2 ? odds + 1 : odds) * (evens > 1&& odds > 2 ? evens  + 1 : evens)
 end
 
-print take_pics(1, 1)
+print take_pics(2, 4)
